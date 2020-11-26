df4fbe691b3e239d123394d3deb1acf26b379ac4
radovanovicmm <markor97radovanovic@gmail.com>
2020-11-26 10:32:52 +0100

    Fixed problems

--- isVip/src/main/java/com/isvip/model/domain/ZahtevZaPodacima.java
+++ isVip/src/main/java/com/isvip/model/domain/ZahtevZaPodacima.java
@@ -19,7 +19,6 @@ public class ZahtevZaPodacima {
     private Zaposleni zaposleni;
     private Klijent klijent;
     private Operator operator;
-    private int i;
 
     public ZahtevZaPodacima() {
     }
